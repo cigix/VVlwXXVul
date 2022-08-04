148cb4552a93fd6c7d0964d837fd985b6217229c
cigix <cigix@cigix.me>
2022-08-04 21:43:10 +0200

    2nd implementation, 200T combinations/s

--- Makefile
+++ Makefile
@@ -13,10 +13,8 @@ codes.txt: codes.py uniques.txt
 binaries.dat: binaries.py codes.txt
 	./binaries.py codes.txt > $@
 
-#matches.txt: match.py codes.txt
-#	match.py codes.txt > $@
 matches.txt: match binaries.dat
-	./match binaries.dat > $@
+	./match binaries.dat | tee $@
 
 clean:
 	$(RM) uniques.txt codes.txt binaries.dat match matches.txt
--- match.c
+++ match.c
@@ -56,38 +56,76 @@ int main(int argc, const char *argv[])
   int codes_len = codes_size / sizeof(*codes);
   fprintf(stderr, "Loaded %d Vlw codes\n", codes_len);
 
+  // Check all codes have popcount == 5
+  fprintf(stderr, "Sanity check... ");
+  for (int i = 0; i < codes_len; ++i)
+  {
+    if (_mm_popcnt_u32(codes[i]) != 5)
+    {
+      fprintf(stderr, "KO: %d\n", i);
+      exit(1);
+    }
+  }
+  fprintf(stderr, "OK\n");
+
   clock_t clock_start = clock();
   int_fast64_t count = 0;
+#define STATUS() \
+  do { \
+    clock_t clock_end = clock(); \
+    double seconds = ((double)(clock_end - clock_start)) / CLOCKS_PER_SEC; \
+    double items_per_second = (double)count / seconds; \
+    fprintf(stderr, "Processed %llu items in %.2fs (%.2e items/s)\r", \
+            (unsigned long long)count, seconds, items_per_second); \
+  } while (0)
 
   for (int id1 = 0; id1 < codes_len; ++id1)
   {
+    STATUS();
     int_fast32_t Vlwcode = codes[id1];
-    for (int id2 = id1; id2 < codes_len; ++id2)
+    for (int id2 = id1 + 1; id2 < codes_len; ++id2)
     {
       int_fast32_t Xlwcode = Vlwcode | codes[id2];
-      for (int id3 = id2; id3 < codes_len; ++id3)
+      if (_mm_popcnt_u32(Xlwcode) != 10)
+      {
+        count += (codes_len - id2) * (codes_len - id2 - 2) * (codes_len - id2 - 3);
+        continue;
+      }
+
+      for (int id3 = id2 + 1; id3 < codes_len; ++id3)
       {
         int_fast32_t XVlwcode = Xlwcode | codes[id3];
-        for (int id4 = id3; id4 < codes_len; ++id4)
+        if (_mm_popcnt_u32(XVlwcode) != 15)
+        {
+          count += (codes_len - id3) * (codes_len - id3 - 1);
+          continue;
+        }
+
+        for (int id4 = id3 + 1; id4 < codes_len; ++id4)
         {
           int_fast32_t XXlwcode = XVlwcode | codes[id4];
-          for (int id5 = id4; id5 < codes_len; ++id5)
+          if (_mm_popcnt_u32(XXlwcode) != 20)
+          {
+            count += codes_len - id4;
+            continue;
+          }
+
+          for (int id5 = id4 + 1; id5 < codes_len; ++id5)
           {
             int_fast32_t XXVlwcode = XXlwcode | codes[id5];
             if (_mm_popcnt_u32(XXVlwcode) == 25)
             {
+              fprintf(stderr, "%64c\r", ' ');
               printf("%#010x,%#010x,%#010x,%#010x,%#010x\n", codes[id1],
                      codes[id2], codes[id3], codes[id4], codes[id5]);
+              fflush(NULL);
             }
             ++count;
           }
         }
       }
-      clock_t clock_end = clock();
-      double seconds = ((double)(clock_end - clock_start)) / CLOCKS_PER_SEC;
-      double items_per_second = (double)count / seconds;
-      fprintf(stderr, "Processed %llu items in %fs (%f items/s)\n",
-              (unsigned long long)count, seconds, items_per_second);
     }
   }
+  STATUS();
+  fprintf(stderr, "\n");
 }
